00f1c3af1f599f6ce1b0c10f449657fc57e0081f
wandou <sbc@pc-133-77.customer.asquith.pinnacle.lan>
2017-12-06 01:21:02 +0000

    finished code in the AddCostViewController

--- Final Project/AddCostViewController.m
+++ Final Project/AddCostViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "AddCostViewController.h"
+#import "DateViewController.h"
+#import "AddressViewController.h"
+#import "ViewController.h"
+#import "Cost.h"
 
 @interface AddCostViewController ()
 
@@ -17,6 +21,67 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.titleBtn setTitle:@"Expense" forState:UIControlStateNormal];
+    [self.titleBtn setTitle:@"Income" forState:UIControlStateSelected];
+}
+
+//update date
+- (void)updateDate:(NSString *)date{
+    [self.dateBtn setTitle:date forState:UIControlStateNormal];
+}
+
+
+//update address
+- (void)updateAddress:(NSString *)address{
+    [self.addressBtn setTitle:address forState:UIControlStateNormal];
+}
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.itemTF resignFirstResponder];
+    [self.valueTF resignFirstResponder];
+}
+
+- (IBAction)titleBtnAction:(UIButton *)sender {
+    sender.selected = !sender.selected;
+}
+
+- (IBAction)confirm:(id)sender {
+    Cost *temCost = [Cost new];
+    temCost.item = self.itemTF.text;
+    temCost.value = self.valueTF.text;
+    temCost.date = self.dateBtn.titleLabel.text;
+    temCost.address = self.addressBtn.titleLabel.text;
+    temCost.type = self.titleBtn.titleLabel.text;
+    temCost.billName = self.billName;
+    if (!temCost.item||!temCost.value||!temCost.date||!temCost.address||!temCost.type) {
+        return;
+    }
+    ViewController *VC = (ViewController *)self.superVC;
+    [VC updateCost:temCost withName:_billName];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+//prepare for segue
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if ([segue.identifier isEqualToString:@"DateViewController"]) {
+        DateViewController *dateVC = (DateViewController *)segue.destinationViewController;
+        dateVC.superVC = self;
+    }
+    else if ([segue.identifier isEqualToString:@"AddressViewController"]) {
+        AddressViewController *addressVC = (AddressViewController *)segue.destinationViewController;
+        addressVC.superVC = self;
+    }
+}
+
+//connecting to the page of choose date
+- (IBAction)selectDate:(id)sender {
+    [self performSegueWithIdentifier:@"DateViewController" sender:nil];
+}
+
+//connecting to the page of input the address
+- (IBAction)selectAddress:(id)sender {
+    [self performSegueWithIdentifier:@"AddressViewController" sender:nil];
 }
 
 - (void)didReceiveMemoryWarning {
